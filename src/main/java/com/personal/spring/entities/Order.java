675002c9d24f1675ae0022323702517f6bacffbe
Bernardo Reis <bernardomreis19@gmail.com>
2024-04-17 11:22:36 +0100

    OrderItem subTotal and Order Total methods

--- src/main/java/com/personal/spring/entities/Order.java
+++ src/main/java/com/personal/spring/entities/Order.java
@@ -96,6 +96,14 @@ public class Order implements Serializable{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double total = 0;
+		for (OrderItem oi : items) {
+			total += oi.getSubTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
